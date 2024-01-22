dea03605eb2d5fb50850b8cbd80b0c7610ee2c09
Žygimantas Jūrelė <labas.as.zygis@gmail.com>
2024-01-22 11:27:17 +0200

    updage

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Food for Thought';
+export const SITE_DESCRIPTION = 'This is a website!';
